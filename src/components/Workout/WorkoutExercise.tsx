fd124d3344c232178ee8318ade3a8bbdfdc20dc0
haigaz15 <haigazsoghomonian@gmail.com>
2024-10-24 18:00:39 +0400

    fixing workout session design

---
+++ src/components/Workout/WorkoutExercise.tsx
@@ -0,0 +1,147 @@
+import {
+  Avatar,
+  Box,
+  Collapse,
+  IconButton,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ExerciseOnWorkout } from "../../types/exercise-types";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useState } from "react";
+import Divider from "@mui/material/Divider";
+
+const WorkoutExercise: React.FC<{ exercise: ExerciseOnWorkout }> = ({
+  exercise,
+}) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const array = new Array(exercise.set).fill(exercise.set);
+  return (
+    <Paper
+      sx={{
+        width: "90%",
+        transition: "height 0.3s ease",
+        marginTop: 2,
+        padding: 1,
+      }}
+    >
+      <Box
+        sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <Avatar
+            alt={exercise.name}
+            src="https://weighttraining.guide/wp-content/uploads/2016/05/barbell-curl-resized.png"
+            sx={{ width: 56, height: 56 }}
+          />
+          <Box sx={{ display: "flex", flexDirection: "column", width: "90%" }}>
+            <Typography variant="inherit" align="left" color="primary.main">
+              {exercise.name}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "1%",
+              }}
+            >
+              <Typography
+                variant="inherit"
+                sx={{ width: "100px" }}
+                align="left"
+              >
+                sets {exercise.set}
+              </Typography>
+              <Typography
+                variant="inherit"
+                sx={{ width: "200px" }}
+                align="left"
+              >
+                reps {exercise.reps}
+              </Typography>
+              <Typography
+                variant="inherit"
+                sx={{ width: "200px" }}
+                align="left"
+              >
+                rest {exercise.rest}
+              </Typography>
+              <Typography
+                variant="inherit"
+                sx={{ width: "300px" }}
+                align="left"
+              >
+                weight {exercise.weight}-140
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+        <IconButton
+          size="small"
+          sx={{
+            "&:hover": {
+              backgroundColor: "inherit", // Prevent background color change on hover
+            },
+          }}
+          onClick={() => setExpanded((prev) => !prev)}
+        >
+          <ExpandMoreIcon
+            sx={{
+              transform: expanded ? "rotate(180deg)" : "rotate(0deg)",
+              transition: "transform 0.3s",
+            }}
+          />
+        </IconButton>
+      </Box>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Divider sx={{ marginTop: "1%" }} />
+        <Box sx={{ marginTop: "1%" }}>
+          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Typography variant="body2" sx={{ width: "80px" }}>
+              Set
+            </Typography>
+            <Typography variant="body2" sx={{ width: "80px" }}>
+              Reps
+            </Typography>
+            <Typography variant="body2" align="left" sx={{ width: "80px" }}>
+              Rest(SEC)
+            </Typography>
+            <Typography variant="body2" align="left" sx={{ width: "80px" }}>
+              Weight(KG)
+            </Typography>
+          </Box>
+        </Box>
+        {array.map((_, index) => {
+          return (
+            <Box
+              key={index}
+              sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                marginTop: "1%",
+              }}
+            >
+              <TextField
+                size="small"
+                sx={{ width: "80px" }}
+                value={index + 1}
+              />
+              <TextField size="small" sx={{ width: "80px" }} value={""} />
+              <TextField size="small" sx={{ width: "80px" }} value={""} />
+              <TextField size="small" sx={{ width: "80px" }} value={""} />
+            </Box>
+          );
+        })}
+      </Collapse>
+    </Paper>
+  );
+};
+
+export default WorkoutExercise;
--- src/pages/Workout/Workout.tsx
+++ src/pages/Workout/Workout.tsx
@@ -6,39 +6,10 @@ import {
   Toolbar,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import React from "react";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
 import WorkoutSummary from "../../components/Workout/WorkoutSummary";
 import WorkoutHead from "../../components/Workout/WorkoutHead";
 import { useStore } from "../../hooks/userStore";
-import ExCard from "../../components/Card/ExCard";
-import WorkoutExerciseCardContent from "../../components/Workout/WorkoutExerciseCardContent";
-
-const workoutBoxStyle = {
-  padding: "8px 16px",
-  backgroundColor: "white",
-  color: "white",
-  borderRadius: "4px",
-  cursor: "pointer",
-  textAlign: "center",
-  boxShadow: 1,
-  "&:hover": {
-    backgroundColor: "#BBDEFB",
-    boxShadow: 2,
-  },
-  "&:active": {
-    backgroundColor: "#1565C0",
-    boxShadow: 0,
-  },
-  width: {
-    xs: "60%",
-    sm: "70%",
-    md: "80%",
-    lg: "90%",
-  },
-};
+import WorkoutExercise from "../../components/Workout/WorkoutExercise";
 
 const Workout = () => {
   const { workoutStore } = useStore();
@@ -47,8 +18,8 @@ const Workout = () => {
       <Toolbar />
       <WorkoutHead />
       {workoutStore.workoutSessions.map((workout) => (
-        <Box style={{ marginLeft: "2%" }}>
-          <Accordion sx={{ width: "1000px", padding: 2 }}>
+        <Box sx={{ marginLeft: "2%" }}>
+          <Accordion sx={{ width: "1100px", padding: 2 }}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
@@ -56,50 +27,12 @@ const Workout = () => {
             >
               <WorkoutSummary workout={workout} />
             </AccordionSummary>
-            <AccordionDetails>
-              <Stepper orientation="vertical">
-                {workout.exercises.map((exercise, index) => (
-                  <Step key={index}>
-                    <Box
-                      onClick={() => console.log("yello")}
-                      sx={{ width: "100%" }}
-                    >
-                      <Box sx={workoutBoxStyle}>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <StepLabel>{exercise.name}</StepLabel>
-                          <ExCard
-                            cardStyle={{
-                              display: "flex",
-                              alignItems: "center",
-                            }}
-                            cardContentStyle={{ flex: "1 0 auto" }}
-                            cardMediaStyle={{
-                              component: "img",
-                              image:
-                                "https://weighttraining.guide/wp-content/uploads/2016/10/dumbbell-shrug-resized.png",
-                              name: exercise.name,
-                              sx: {
-                                height: 200,
-                                objectFit: "contain",
-                                width: 400, // Adjust width to fit your layout
-                                flexShrink: 0, // Prevent the image from shrinking
-                              },
-                            }}
-                            cardContentLayout={() => (
-                              <WorkoutExerciseCardContent exercise={exercise} />
-                            )}
-                          />
-                        </Box>
-                      </Box>
-                    </Box>
-                  </Step>
-                ))}
-              </Stepper>
+            <AccordionDetails sx={{ maxHeight: 800, overflowY: "auto" }}>
+              {workout.exercises.map((exercise) => {
+                return (
+                  <WorkoutExercise key={exercise.name} exercise={exercise} />
+                );
+              })}
             </AccordionDetails>
           </Accordion>
         </Box>
